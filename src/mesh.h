6aa11318453cee12613e36d0cce9a7133745b613
Axel Wettervik <axel.wettervik@gmail.com>
2017-01-26 01:30:17 +0900

    changed mesh struct declaration style.

--- src/mesh.h
+++ src/mesh.h
@@ -26,41 +26,47 @@ enum vertex_attributes
 #define VA_INCLUDE(mask, bits) (mask |= bits)
 #define VA_EXCLUDE(mask, bits) (mask ^= bits)
 
-typedef struct
+typedef struct color color;
+typedef struct vertex_data vertex_data;
+typedef struct mesh_data mesh_data;
+typedef struct input_layout input_layout;
+typedef struct loaded_mesh loaded_mesh;
+
+struct color
 {
     float r;
     float g;
     float b;
-} color;
+};
 
-typedef struct
+struct vertex_data
 {
     vector3* positions;
     vector3* normals;
     vector2* texcoords;
     color* colors;
-} vertex_data;
+};
 
-typedef struct
+struct mesh_data
 {
     int vertex_count;
     vertex_data vertices;
-} mesh_data;
+};
 
-typedef struct
+struct input_layout
 {
     int attribute_mask;
     GLuint vertex_array;
-} input_layout;
+};
 
-typedef struct
+struct loaded_mesh
 {
     int attribute_mask;
     int layout_count;
     input_layout layouts[MESH_MAX_INPUT_LAYOUTS];
     GLuint vertex_buffer[MESH_MAX_VERTEX_BUFFERS];
     mesh_data data;
-} loaded_mesh;
+};
 
 loaded_mesh load_mesh(gl_functions* gl, mesh_data data);
 
