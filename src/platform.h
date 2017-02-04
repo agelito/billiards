2a1fafda72d239d5f9c8a54e117c0d3346fccdb4
Axel Wettervik <axel.wettervik@gmail.com>
2017-02-04 21:57:43 +0900

    Formatting fix to make platform.h more readable.

--- src/platform.h
+++ src/platform.h
@@ -22,22 +22,31 @@ struct read_file
     unsigned char* data;
 };
 
-long platform_executable_directory(char* destination, long destination_size);
+long
+platform_executable_directory(char* destination, long destination_size);
 
-void platform_set_working_directory(char* directory);
+void
+platform_set_working_directory(char* directory);
 
-void platform_sleep(int milliseconds);
+void
+platform_sleep(int milliseconds);
 
-read_file platform_read_file(char* path, int append_null);
+read_file
+platform_read_file(char* path, int append_null);
 
-void platform_free_file(read_file* file);
+void
+platform_free_file(read_file* file);
 
-void platform_random_seed(int seed);
+void
+platform_random_seed(int seed);
 
-int platform_random(int min, int max);
+int
+platform_random(int min, int max);
 
-float platform_randomf(float min, float max);
+float
+platform_randomf(float min, float max);
 
-void platform_copy_memory(void* destination, void* source, long size);
+void
+platform_copy_memory(void* destination, void* source, long size);
 
 #endif // PLATFORM_H_INCLUDED
